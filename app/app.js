d3ce0f0ed5baa18223b424c88bc9a302c49a3cf9
aehmt <calisahmet@gmail.com>
2017-02-28 22:47:21 -0500

    form section added

--- app/app.js
+++ app/app.js
@@ -9,9 +9,14 @@ app.set('appData', dataFile);
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
+app.locals.siteTitle = 'Roux Meetups'
+app.locals.allSpeakers = dataFile.speakers;
+
 app.use(express.static('app/public'));
 app.use(require('./routes/index'));
 app.use(require('./routes/speakers'));
+app.use(require('./routes/feedback'));
+app.use(require('./routes/api'));
 
 var server = app.listen(app.get('port'), function() {
   console.log('Listening on port ' + app.get('port'));
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('index')
+  var data = req.app.get('appData');
+  var pagePhotos = [];
+  var pageSpeakers = data.speakers;
+
+  data.speakers.forEach((item) => {
+    pagePhotos = pagePhotos.concat(item.artwork); 
+  });
+
+  res.render('index', {
+    pageTitle: 'Home',
+    pageID: 'home',
+    artwork: pagePhotos,
+    speakers: pageSpeakers,
+  })
 });
 
 module.exports = router;
